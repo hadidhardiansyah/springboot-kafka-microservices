d523434f752a45d2760f83750d2967375979c242
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-11-30 11:18:08 +0700

    Initial project

---
+++ order-service/src/test/java/com/hadid/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadid.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
